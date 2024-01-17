1f1e0b415e1b0bfc50102e4c5225a67c92955f6c
FiqSho <119481593+FiqSho@users.noreply.github.com>
2024-01-17 19:06:49 +0600

    adding student data in result.js

--- src/Components/StudentsTable/result.js
+++ src/Components/StudentsTable/result.js
@@ -25,6 +25,62 @@ const studentInfo = [{
     Class: "One"
 },
 {
+    "ID": 4,
+    "Img": studentImg,
+    "Name": "Alex",
+    "Scores": "A",
+    "Percentage": "85%",
+    "Class": "One"
+},
+{
+    "ID": 5,
+    "Img": studentImg,
+    "Name": "Sara",
+    "Scores": "B+",
+    "Percentage": "78%",
+    "Class": "One"
+},
+{
+    "ID": 6,
+    "Img": studentImg,
+    "Name": "Michael",
+    "Scores": "C+",
+    "Percentage": "65%",
+    "Class": "One"
+},
+{
+    "ID": 7,
+    "Img": studentImg,
+    "Name": "Emily",
+    "Scores": "A-",
+    "Percentage": "90%",
+    "Class": "One"
+},
+{
+    "ID": 8,
+    "Img": studentImg,
+    "Name": "Daniel",
+    "Scores": "B",
+    "Percentage": "75%",
+    "Class": "One"
+},
+{
+    "ID": 9,
+    "Img": studentImg,
+    "Name": "Olivia",
+    "Scores": "A+",
+    "Percentage": "92%",
+    "Class": "One"
+},
+{
+    "ID": 10,
+    "Img": studentImg,
+    "Name": "Ethan",
+    "Scores": "C",
+    "Percentage": "68%",
+    "Class": "One"
+},
+{
     ID: 1,
     Img: studentImg,
     Name: "Shofiq",
@@ -48,6 +104,63 @@ const studentInfo = [{
     Percentage: "75%",
     Class: "Two"
 },
+{
+    "ID": 4,
+    "Img": studentImg,
+    "Name": "Liam",
+    "Scores": "B-",
+    "Percentage": "80%",
+    "Class": "Two"
+},
+{
+    "ID": 5,
+    "Img": studentImg,
+    "Name": "Mia",
+    "Scores": "A",
+    "Percentage": "88%",
+    "Class": "Two"
+},
+{
+    "ID": 6,
+    "Img": studentImg,
+    "Name": "Jackson",
+    "Scores": "C+",
+    "Percentage": "72%",
+    "Class": "Two"
+},
+{
+    "ID": 7,
+    "Img": studentImg,
+    "Name": "Sophia",
+    "Scores": "A-",
+    "Percentage": "87%",
+    "Class": "Two"
+},
+{
+    "ID": 8,
+    "Img": studentImg,
+    "Name": "Logan",
+    "Scores": "B",
+    "Percentage": "79%",
+    "Class": "Two"
+},
+{
+    "ID": 9,
+    "Img": studentImg,
+    "Name": "Ava",
+    "Scores": "A+",
+    "Percentage": "94%",
+    "Class": "Two"
+},
+{
+    "ID": 10,
+    "Img": studentImg,
+    "Name": "Noah",
+    "Scores": "C",
+    "Percentage": "66%",
+    "Class": "Two"
+}
+
 ]
 
 export default studentInfo
